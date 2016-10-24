2fd0968e644c7751952311d1205d6186b84ed050
Artyom Veremeenko <arwer13@gmail.com>
2016-10-24 13:45:00 +0300

    add skeleton of loopback application

---
+++ server/boot/create_models.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function(app) {
+//  app.dataSources.postgresTest.automigrate('garden', function(err) {
+//    if (err) throw err;
+//
+//    app.models.garden.create([
+//        {name: 'oak'},
+//        {name: 'apple', description: 'some apple tree'}
+//    ], function(err, garden) {
+//      if (err) throw err;
+//
+//      console.log('Models created: \n', garden);
+//    });
+//  });
+};
+
